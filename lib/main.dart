d2260ab8e74ae10167cea9b3d232bdfa9b6a0a5b
hemachadran <hemachandran120200@gmail.com>
2020-04-06 19:43:02 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:social_graph/form.dart';
-import 'package:social_graph/home.dart';
 import 'package:social_graph/login.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Social Graph",
-    home: Home(),
+    home: LoginPage(),
   ));
 }
